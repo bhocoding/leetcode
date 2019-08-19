2a1197a659bcd4558994666a1615dcff6f90bd9a
bhocoding <srinisam@gmail.com>
2019-08-18 23:25:56 -0500

    minor changes to comments and variable names

--- leet388.py
+++ leet388.py
@@ -1,31 +1,32 @@
-def lengthLongestPath(self, input):
-	# 2 pointer approach
-	def advance(begin, input):
-		level = 0
-		while begin < len(input) and input[begin] != '\n':
-			if input[begin] == '\t':
-				level += 1
-			begin += 1
-		return level,begin
-	stack = []
-	begin = 0
-	level = 0
-	max_length = 0
-	while begin < len(input):
-		# advance till the next '\n'
-		level,end = advance(begin,input)
-		# walk up the directory tree
-		while len(stack) > level:
-			stack.pop()
-		# dir or file string length
-		length = end-begin-level
-		if stack:
-			length += 1 # add 1 for delimiter '\'
-		# if file calculate path length, else add to path
-		if '.' in input[begin:end]:
-			max_length = max(max_length, sum(stack) + length)
-		else:
-			stack.append(length)
-		# advance pointer to start from position after '\n'
-		begin = end + 1
-	return max_length
+class Solution:
+	def lengthLongestPath(self, input):
+		# 2 pointer approach
+		def advance(ptr, input):
+			level = 0
+			while ptr < len(input) and input[ptr] != '\n':
+				if input[ptr] == '\t':
+					level += 1
+				ptr += 1
+			return level,ptr
+		stack = []
+		begin = 0
+		num_tab = 0
+		max_length = 0
+		while begin < len(input):
+			# advance till the next '\n'
+			num_tab,end = advance(begin,input)
+			# walk up the directory tree, if necessary
+			while len(stack) > num_tab:
+				stack.pop()
+			# dir/file string length
+			length = end-begin-num_tab
+			if stack:
+				length += 1 # add 1 for delimiter '\'
+			# is this a file or dir?
+			if '.' in input[begin:end]:
+				max_length = max(max_length, sum(stack) + length)
+			else:
+				stack.append(length)
+			# advance pointer to start from position after '\n'
+			begin = end + 1
+		return max_length
